0b51e8b055eebcdd11c562f6391843d25bfab9c8
Kraig Brockschmidt <kraigbr@msn.com>
2018-04-16 16:14:19 -0700

    Create initial Django app

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,12 +3,16 @@ Definition of urls for BasicProject.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home')
+
     # Examples:
     # url(r'^$', BasicProject.views.home, name='home'),
     # url(r'^BasicProject/', include('BasicProject.BasicProject.urls')),
