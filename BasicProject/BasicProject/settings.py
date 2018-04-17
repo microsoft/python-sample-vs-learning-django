1207b569f5ab960d57f1212df63522d63b908173
Kraig Brockschmidt <kraigbr@msn.com>
2018-04-16 17:01:31 -0700

    Update for tutorial step 2

--- BasicProject/BasicProject/settings.py
+++ BasicProject/BasicProject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Add your apps here to enable them
+    'HelloDjangoApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse  # For tutorial steps 2-1 and 2-2
+from datetime import datetime         # For tutorial step 2-2
+from django.shortcuts import render   # For tutorial step 2-3
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    # Code for tutorial step 2-1
+    # return HttpResponse("Hello, Django!")
+
+    # Code for tutorial step 2-2    
+    #now = datetime.now()
+
+    #html_content = "<html><head><title>Hello, Django</title></head><body>"
+    #html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    #html_content += "</body></html>"
+
+    #return HttpResponse(html_content)
+    
+    # Code for tutorial step 2-3
+    now = datetime.now()
+
+    return render(
+        request,
+        "index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
